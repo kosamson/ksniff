95e4a3bb0642b16e55cf500547393025a8e8fd37
kosamson <kosamson1@gmail.com>
2024-09-08 20:54:30 -0700

    add env var viper bind check to unit test

--- pkg/cmd/sniff_test.go
+++ pkg/cmd/sniff_test.go
@@ -8,50 +8,56 @@ import (
 	"ksniff/pkg/config"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/cli-runtime/pkg/genericiooptions"
 )
 
 func TestNewCmdSniff_FlagsRegistered(t *testing.T) {
-	t.Parallel()
-
 	assert := assert.New(t)
 
 	flagKeys := []struct {
 		name       string
 		shorthand  string
 		defaultVal string
+		envKey     string
 	}{
-		{"namespace", "n", ""},
-		{"interface", "i", "any"},
-		{"container", "c", ""},
-		{"filter", "f", ""},
-		{"output-file", "o", ""},
-		{"local-tcpdump-path", "l", ""},
-		{"remote-tcpdump-path", "r", "/tmp/static-tcpdump"},
-		{"verbose", "v", "false"},
-		{"privileged", "p", "false"},
-		{"pod-creation-timeout", "", "1m0s"},
-		{"image", "", ""},
-		{"tcpdump-image", "", ""},
-		{"context", "x", ""},
-		{"socket", "", ""},
-		{"serviceaccount", "s", ""},
+		{"namespace", "n", "", "KUBECTL_PLUGINS_CURRENT_NAMESPACE"},
+		{"interface", "i", "any", "KUBECTL_PLUGINS_LOCAL_FLAG_INTERFACE"},
+		{"container", "c", "", "KUBECTL_PLUGINS_LOCAL_FLAG_CONTAINER"},
+		{"filter", "f", "", "KUBECTL_PLUGINS_LOCAL_FLAG_FILTER"},
+		{"output-file", "o", "", "KUBECTL_PLUGINS_LOCAL_FLAG_OUTPUT_FILE"},
+		{"local-tcpdump-path", "l", "", "KUBECTL_PLUGINS_LOCAL_FLAG_LOCAL_TCPDUMP_PATH"},
+		{"remote-tcpdump-path", "r", "/tmp/static-tcpdump", "KUBECTL_PLUGINS_LOCAL_FLAG_REMOTE_TCPDUMP_PATH"},
+		{"verbose", "v", "false", "KUBECTL_PLUGINS_LOCAL_FLAG_VERBOSE"},
+		{"privileged", "p", "false", "KUBECTL_PLUGINS_LOCAL_FLAG_PRIVILEGED"},
+		{"pod-creation-timeout", "", "1m0s", ""},
+		{"image", "", "", "KUBECTL_PLUGINS_LOCAL_FLAG_IMAGE"},
+		{"tcpdump-image", "", "", "KUBECTL_PLUGINS_LOCAL_FLAG_TCPDUMP_IMAGE"},
+		{"context", "x", "", "KUBECTL_PLUGINS_CURRENT_CONTEXT"},
+		{"socket", "", "", "KUBECTL_PLUGINS_SOCKET_PATH"},
+		{"serviceaccount", "s", "", "KUBECTL_PLUGINS_LOCAL_FLAG_SERVICE_ACCOUNT"},
 	}
 
 	cmd := cmd.NewCmdSniff(genericiooptions.NewTestIOStreamsDiscard())
 
 	for _, flagKey := range flagKeys {
 		t.Run(flagKey.name, func(t *testing.T) {
-			t.Parallel()
-
 			flag := cmd.Flags().Lookup(flagKey.name)
 
 			assert.NotNil(flag)
 			assert.Equal(flagKey.name, flag.Name)
 			assert.Equal(flagKey.shorthand, flag.Shorthand)
 			assert.Equal(flagKey.defaultVal, flag.DefValue)
+
+			// check if viper keys set properly based on flag name
+			if flagKey.envKey != "" {
+				envVal := "test-value-" + flagKey.name
+
+				t.Setenv(flagKey.envKey, envVal)
+				assert.Equal(envVal, viper.GetString(flagKey.name))
+			}
 		})
 	}
 }
